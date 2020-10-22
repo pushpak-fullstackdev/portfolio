b02bcec5489f371d42f2309968df7947c8c17a23
Pushpak Singirikonda <pushpak@Pushpaks-MacBook-Pro.local>
2020-10-22 21:49:16 +0530

    Home page completed

---
+++ precache-manifest.bbf32bffb7ec3d0913dc67db7300a17a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d4d514466851897b4f68e98f0430ea",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "eaf337cbcede472477ee",
+    "url": "/portfolio/static/css/main.50709cce.chunk.css"
+  },
+  {
+    "revision": "5c601428e66077ed97a0",
+    "url": "/portfolio/static/js/2.daaebc6f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.daaebc6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf337cbcede472477ee",
+    "url": "/portfolio/static/js/main.2fe79d50.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7637b49efb4a90170b6fd51b7da4ecc2.js"
+  "/portfolio/precache-manifest.bbf32bffb7ec3d0913dc67db7300a17a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
