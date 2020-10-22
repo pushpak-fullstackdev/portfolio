6f5800dfe1a43589b9bae201d25d53f8a4aaf75e
Pushpak Singirikonda <pushpak@Pushpaks-MacBook-Pro.local>
2020-10-22 21:21:11 +0530

    Home page completed

---
+++ build/precache-manifest.7637b49efb4a90170b6fd51b7da4ecc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0b8cbcdadf77fe60748ccfd0ac0016",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5145e9216f6d0b127d16",
+    "url": "/static/css/main.50709cce.chunk.css"
+  },
+  {
+    "revision": "5c601428e66077ed97a0",
+    "url": "/static/js/2.daaebc6f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.daaebc6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5145e9216f6d0b127d16",
+    "url": "/static/js/main.c2e52a82.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
