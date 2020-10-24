6bf6f9b683a788e182d8b9e40a80da0e78cd8eca
Pushpak Singirikonda <pushpak@Pushpaks-MacBook-Pro.local>
2020-10-24 10:46:29 +0530

    added changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a711e9d847173eaa20869d2ff8b19430.js"
+  "/portfolio/precache-manifest.37c50444a08e056a8489de2fc51234a2.js"
 );
 
 self.addEventListener('message', (event) => {
